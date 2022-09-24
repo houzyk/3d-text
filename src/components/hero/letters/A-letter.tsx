0a5beb8f9f56b1262c7233ad54b231fc0c5f3f1a
Houzair Koussa <houzairmk@icloud.com>
2022-09-24 19:54:33 +0400

    [dev] ADD leter rendering

--- src/components/hero/letters/A-letter.tsx
+++ src/components/hero/letters/A-letter.tsx
@@ -1,10 +1,18 @@
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import React, { useRef, useState } from 'react';
+import { useFrame, extend } from '@react-three/fiber'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
+import roboto from "../../../assets/Roboto.json";
+
+extend({ TextGeometry });
 
 const ALetter = (props: any) => {
   const ref = useRef();
   const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
+
+  const font = new FontLoader().parse(roboto);
+
   useFrame((state, delta) => {
     const letterRef = ref.current as any;
     if (letterRef) {
@@ -19,9 +27,10 @@ const ALetter = (props: any) => {
       scale={clicked ? 1.5 : 1}
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[0.8, 0.8, 0.8]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      onPointerOut={(event) => hover(false)}
+    >
+      <textGeometry args={['A', {font, size: 1, height: 0.5}]}/>
+      <meshLambertMaterial attach='material' color={'gold'}/>
     </mesh>
   );
 }
--- src/components/hero/letters/Z-letter.tsx
+++ src/components/hero/letters/Z-letter.tsx
@@ -1,14 +1,22 @@
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import React, { useRef, useState } from 'react';
+import { useFrame, extend } from '@react-three/fiber'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
+import roboto from "../../../assets/Roboto.json";
+
+extend({ TextGeometry });
 
 const ZLetter = (props: any) => {
   const ref = useRef();
   const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
+
+  const font = new FontLoader().parse(roboto);
+
   useFrame((state, delta) => {
     const letterRef = ref.current as any;
     if (letterRef) {
-      // letterRef.rotation.x += 0.01
+      // letterRef.rotation.y += 0.01
     }
   });
 
@@ -19,9 +27,10 @@ const ZLetter = (props: any) => {
       scale={clicked ? 1.5 : 1}
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[0.8, 0.8, 0.8]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      onPointerOut={(event) => hover(false)}
+    >
+      <textGeometry args={['Z', {font, size: 1, height: 0.5}]}/>
+      <meshLambertMaterial attach='material' color={'gold'}/>
     </mesh>
   );
 }
