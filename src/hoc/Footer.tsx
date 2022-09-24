b0a019a30a2b55419346fc91862b00a54cad7c13
Houzair Koussa <houzairmk@icloud.com>
2022-09-24 18:37:15 +0400

    [dev] ADD sections component hoc

---
+++ src/hoc/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from "styled-components"
+
+const Footer = () => {
+  return (
+    <StyleWrapper>
+    </StyleWrapper>
+  );
+}
+
+const StyleWrapper = styled.div``
+
+export default Footer;
---
+++ src/hoc/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from "styled-components"
+
+const Header = () => {
+  return (
+    <StyleWrapper>
+    </StyleWrapper>
+  );
+}
+
+const StyleWrapper = styled.nav``
+
+export default Header;
