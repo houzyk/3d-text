255cd901bb8670330d099490af6be0d9efcfeec1
Houzair Koussa <houzairmk@icloud.com>
2022-09-24 20:39:30 +0400

    [refac] Hero letter rendering

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,16 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import styled from "styled-components"
 import { Canvas } from '@react-three/fiber'
 
-import HLetter from "./letters/H-letter";
-import OLetter from "./letters/O-letter";
-import ULetter from "./letters/U-letter";
-import ZLetter from "./letters/Z-letter";
-import ALetter from "./letters/A-letter";
-import ILetter from "./letters/I-letter";
-import RLetter from "./letters/R-letter";
+import Letter from "./Letter";
 
 const Hero = () => {
+
+  const letters = useMemo(() => ['H', 'O', 'U', 'Z', 'A', 'I', 'R'], []);
 
   const [hLetterPosition, setHLetterPosition] = useState([0, 3, 0]);
   const [oLetterPosition, setOLetterPosition] = useState([0, 2, 0]);
@@ -35,6 +31,7 @@ const Hero = () => {
       setHLetterPosition([0, 3, 0]);
       setOLetterPosition([0, 2, 0]);
       setULetterPosition([0, 1, 0]);
+      setZLetterPosition([0, 0, 0]);
       setALetterPosition([0, -1, 0]);
       setILetterPosition([0, -2, 0]);
       setRLetterPosition([0, -3, 0]);
@@ -42,24 +39,31 @@ const Hero = () => {
       setHLetterPosition([-3, 0, 0]);
       setOLetterPosition([-2, 0, 0]);
       setULetterPosition([-1, 0, 0]);
+      setZLetterPosition([0, 0, 0]);
       setALetterPosition([1, 0, 0]);
       setILetterPosition([2, 0, 0]);
       setRLetterPosition([3, 0, 0]);
     }
   }
 
+  const letterPositions = [
+    hLetterPosition,
+    oLetterPosition,
+    uLetterPosition,
+    zLetterPosition,
+    aLetterPosition,
+    iLetterPosition,
+    rLetterPosition
+  ]
+
   return (
     <StyleWrapper className="hero-section">
       <Canvas>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <HLetter position={hLetterPosition} />
-        <OLetter position={oLetterPosition} />
-        <ULetter position={uLetterPosition} />
-        <ZLetter position={zLetterPosition} />
-        <ALetter position={aLetterPosition} />
-        <ILetter position={iLetterPosition} />
-        <RLetter position={rLetterPosition} />
+        {letters.map((letter, index) => (
+          <Letter position={letterPositions[index]} letter={letter}/>
+        ))}
       </Canvas>
     </StyleWrapper>
   );
