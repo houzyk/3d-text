4601386e61b5f394cf285b5430b6abb2fe5a24e9
Houzair Koussa <houzairmk@icloud.com>
2022-09-24 18:36:47 +0400

    [dev] ADD roots for sections

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
-const rootDiv = document.getElementById('root') as HTMLDivElement;
+import GlobalStyle from './styles/globalStyles';
 
-if (rootDiv) {
-  const root = ReactDOM.createRoot(rootDiv);
-  root.render(
+import App from './hoc/App';
+import Header from './hoc/Header';
+import Footer from './hoc/Footer';
+
+const main = document.getElementById('root-main') as HTMLElement;
+const header = document.getElementById('root-header') as HTMLElement;
+const footer = document.getElementById('root-footer') as HTMLElement;
+
+if (main) {
+  const mainRoot = ReactDOM.createRoot(main);
+  mainRoot.render(
     <React.StrictMode>
+      <GlobalStyle />
       <App />
     </React.StrictMode>
   );
 }
 
+if (header) {
+  const headerRoot = ReactDOM.createRoot(header);
+  headerRoot.render(
+    <React.StrictMode>
+      <GlobalStyle />
+      <Header />
+    </React.StrictMode>
+  );
+}
 
+if (footer) {
+  const footerRoot = ReactDOM.createRoot(footer);
+  footerRoot.render(
+    <React.StrictMode>
+      <GlobalStyle />
+      <Footer />
+    </React.StrictMode>
+  );
+}
